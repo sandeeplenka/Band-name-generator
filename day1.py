7c52093716d1004a8be3875be6356a8fcf198716
Sandeep kumar lenka <60995366+sandeeplenka@users.noreply.github.com>
2023-02-25 18:25:24 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,7 @@
+print("👋Hello👋")
+print("Welcome to the Band Name Generator.")
+city=input("What's name of the city that you grew up in?\n")
+pet_name=input("What's name of your pet?\n")
+print("Band name could be: "+city+" "+pet_name)
+
+
